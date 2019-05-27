1f522592b8c055d59d144ae35c733a6ca951cf89
Raphael Erik Hviding <raphael.hviding@gmail.com>
2019-05-27 09:19:38 +0200

    Update HQUAILS.py

--- HQUAILS.py
+++ HQUAILS.py
@@ -3,22 +3,24 @@ import scipy.stats as stats
 import numpy as np
 from astropy.modeling import models, fitting, custom_model
 
+# Temporary packages
 import matplotlib.pyplot as plt
+from importlib import reload
 
 
 # HQUAILS supporting files
-from RegionFinding import identifyComplexes
-
+import RegionFinding
+reload(RegionFinding)
 # emissionLines: Emission Lines Dictionary
 
 
 # Emission line dictionary
 emissionLines = {
 'Narrow':{
-	'OIII':([(5006.77,1),(4958.83,0.350)],1,('Broad')),
-	'Hbeta':([(4861.32,1)],1,('Broad')),
-	'NII':([(6583.34,1),(6547.96,0.340)],1,('Broad')),
-	'Halpha':([(6562.80,1)],1,('Broad'))},
+	'OIII':([(5006.77,1),(4958.83,0.350)],1,['Broad']),
+	'Hbeta':([(4861.32,1)],1,['Broad']),
+	'NII':([(6583.34,1),(6547.96,0.340)],1,['Broad']),
+	'Halpha':([(6562.80,1)],1,['Broad'])},
 'Broad':{}
 }
 
@@ -61,11 +63,20 @@ y += np.random.normal(0., sigmas, x.shape)
 # Main Function
 def HQUAILS(emissionLines,region_width=50):
 
-	## Iterate over spectra (eventually)
+	# Verify Emission Line Dictionary
+	if not verifyDict(emissionLines):
+		print('Unable to verify emission line dictionary, exiting.')
+		return
+	print('Emission line dictionary verified.')
+
+	## Identify fitting regions
+	regions = RegionFinding.identifyComplexes(emissionLines,tol=region_width)
+	print('Identififed emission line complexes.')
+
+# Iterate over spectra (eventually)
 	wav,flux,weight,z = x,y,weights,0
 	
-	## Identify fitting regions
-	regions = identifyComplexes(emissionLines,tol=region_width)
+
 	
 	# Check if there is spectral coverage of the regions
 	for region in regions:
@@ -78,7 +89,74 @@ def HQUAILS(emissionLines,region_width=50):
 	
 	
 	print(regions)
+# Verify if emission line dictionary is in correct format. 
+def verifyDict(emissionLines):
+
+	# Check if Emission Lines are in dictionary
+	if not type(emissionLines) == dict:
+		print('Emission lines must be in dictionary.')
+		return False
+	
+	# Check if redshift group is in a dictionary
+	for z in emissionLines.keys():
+		if not type(emissionLines[z]) == dict:
+			print('Each redshift must be a dictionary.')
+			return False
+		if not type(z) == str:
+			print('Redshift key must be a string.')
+			return False
 		
+		# Check if species is in an appropriate tuple or list
+		for species in emissionLines[z]:
+			if not ((type(emissionLines[z][species]) == tuple) or \
+					(type(emissionLines[z][species]) == list)):
+				print('Each species must be a tuple or list.')
+				return False
+			if not type(species) == str:
+				print('Species key must be a string.')
+				return False
+			if not len(emissionLines[z][species]) == 3:
+				print('Species must be length 3.')
+				return False
+			# Check if lines are in a list or tuple
+			if not ((type(emissionLines[z][species][0]) == list) or \
+					(type(emissionLines[z][species][0]) == tuple)):
+				print('Lines must be in a list or tuple.')
+				return False
+				
+			# Check if flag is an good int
+			if not type(emissionLines[z][species][1]) == int:
+				print('Lines flag must be an int.')
+				return False
+			if not emissionLines[z][species][1] >= 0:
+				print('Lines flag must be a positive int.')
+				return False
+				
+			# Check if additional components are in a list 
+			if not (type(emissionLines[z][species][2]) == list):
+				print('Line component redshift groups must be a list.')
+				return False
+							
+			# Check if each line is an appropriate tuple or list
+			for line in emissionLines[z][species][0]:
+				if not (type(line) == tuple) or (type(line) == list):
+					print('Each line must be a tuple or list.')
+					return False
+				if not len(line) == 2:
+					print('Each line tuple must have two elements.')
+					return False
+				for l in line:
+					if not ((type(l) == float) or (type(l) == int)):
+						print('Each element of tuple must be a float or int.')
+						return False
+			
+			# Check if flag bit sum is equal to length of 
+			if not (sum([int(bit) for bit in bin(emissionLines[z][species][1])[2:]]) == \
+					len(emissionLines[z][species][2])):
+				print('Flag number does not match number of additional components.')
+				return False
+						
+	return True
 
 HQUAILS(emissionLines)
 	
