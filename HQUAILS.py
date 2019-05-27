59e08643b9fc635a3351f022cd466bb75fbae07f
Raphael Erik Hviding <raphael.hviding@gmail.com>
2019-05-27 16:37:39 +0200

    Created First Custom Model

--- HQUAILS.py
+++ HQUAILS.py
@@ -1,12 +1,10 @@
-import scipy
-import scipy.stats as stats
-import numpy as np
-from astropy.modeling import models, fitting, custom_model
+import copy
 
 # Temporary packages
 import matplotlib.pyplot as plt
 from importlib import reload
-
+import numpy as np
+from astropy.modeling import models, fitting, custom_model
 
 # HQUAILS supporting files
 import RegionFinding
@@ -36,13 +34,13 @@ def emissionLine(x,redshift=0,height=0,sigma=1,center=0):
 
 # Define linear background custom model
 @custom_model
-def background(x,slope,intercept,rightedge,leftedge):
+def background(x,slope=0,intercept=1,rightedge=0,leftedge=0):
 	center 	= (rightedge + leftedge)/2
 	y 		= np.zeros(x.shape)
 	y[np.logical_and(x > rightedge,x < leftedge)] = slope*(x - center) + intercept
 	
 ## Fake spectrum
-x = np.arange(4000,7000,1) # Angstroms
+x = np.arange(6000,7000,1) # Angstroms
 y = 0.5*np.ones(x.shape) # Flux
 # Balmer
 y += models.Gaussian1D(amplitude=1, mean=6562.80, stddev=5)(x)
@@ -61,22 +59,25 @@ y += np.random.normal(0., sigmas, x.shape)
 
 
 # Main Function
-def HQUAILS(emissionLines,region_width=50):
+def HQUAILS(emissionLines_master,region_width=50):
 
 	# Verify Emission Line Dictionary
-	if not verifyDict(emissionLines):
+	if not verifyDict(emissionLines_master):
 		print('Unable to verify emission line dictionary, exiting.')
 		return
 	print('Emission line dictionary verified.')
 
 	## Identify fitting regions
-	regions = RegionFinding.identifyComplexes(emissionLines,tol=region_width)
+	regions_master = RegionFinding.identifyComplexes(emissionLines_master,tol=region_width)
 	print('Identififed emission line complexes.')
 
 # Iterate over spectra (eventually)
 	wav,flux,weight,z = x,y,weights,0
 	
-
+	## Seting up regions list and emissionLines dictionary for this spectrum ##
+	# Deep copy region list and emission line dictionary for this spectrum
+	regions 		= copy.deepcopy(regions_master)
+	emissionLines 	= copy.deepcopy(emissionLines_master)
 	
 	# Check if there is spectral coverage of the regions
 	for region in regions:
@@ -84,11 +85,36 @@ def HQUAILS(emissionLines,region_width=50):
 		if (wav.min() > region[0]*(1+z)) or (wav.max() < region[1]*(1+z)):
 			# ...remove region
 			regions.remove(region)
+		
+	# Remove emission lines not in regions
+	# For each emission line	
+	for z in emissionLines_master.keys():
+		for species in emissionLines_master[z].keys():
+			for line in emissionLines_master[z][species][0]:
+
+				# Check for removal
+				remove = True # Initialize as removing
+				for region in regions: # Check if it is in any region
+					if (region[0] < line[0]) and (line[0] < region[1]):
+						remove = False # If it is, set as remove 
+				
+				# Remove if necessary
+				if remove:
+					emissionLines[z][species][0].remove(line)
 			
-	## Moodel Constructor
+			# If species is empty, delete it
+			if (len(emissionLines[z][species][0]) == 0):
+				del emissionLines[z][species]		
+	## Seting up regions list and emissionLines dictionary for this spectrum ##
+
+	## Model construction ##
+	
+	## Model construction ##
 	
 	
-	print(regions)
+	print(regions,emissionLines)
+			
+	## Model Constructor
 # Verify if emission line dictionary is in correct format. 
 def verifyDict(emissionLines):
 
@@ -112,23 +138,22 @@ def verifyDict(emissionLines):
 					(type(emissionLines[z][species]) == list)):
 				print('Each species must be a tuple or list.')
 				return False
-			if not type(species) == str:
+			if not (type(species) == str):
 				print('Species key must be a string.')
 				return False
-			if not len(emissionLines[z][species]) == 3:
+			if not (len(emissionLines[z][species]) == 3):
 				print('Species must be length 3.')
 				return False
 			# Check if lines are in a list or tuple
-			if not ((type(emissionLines[z][species][0]) == list) or \
-					(type(emissionLines[z][species][0]) == tuple)):
-				print('Lines must be in a list or tuple.')
+			if not (type(emissionLines[z][species][0]) == list):
+				print('Lines must be in a list.')
 				return False
 				
 			# Check if flag is an good int
-			if not type(emissionLines[z][species][1]) == int:
+			if not (type(emissionLines[z][species][1]) == int):
 				print('Lines flag must be an int.')
 				return False
-			if not emissionLines[z][species][1] >= 0:
+			if not (emissionLines[z][species][1] >= 0):
 				print('Lines flag must be a positive int.')
 				return False
 				
@@ -136,13 +161,17 @@ def verifyDict(emissionLines):
 			if not (type(emissionLines[z][species][2]) == list):
 				print('Line component redshift groups must be a list.')
 				return False
-							
+			for component in emissionLines[z][species][2]:
+				if not component in emissionLines.keys():
+					print('Additional component redshift must be in emission dictionary.')
+					return False
+					
 			# Check if each line is an appropriate tuple or list
 			for line in emissionLines[z][species][0]:
 				if not (type(line) == tuple) or (type(line) == list):
 					print('Each line must be a tuple or list.')
 					return False
-				if not len(line) == 2:
+				if not (len(line) == 2):
 					print('Each line tuple must have two elements.')
 					return False
 				for l in line:
--- custommodels.py
+++ custommodels.py
@@ -1,62 +1,113 @@
-import scipy
-import scipy.stats as stats
 import numpy as np
-import matplotlib.pyplot as plt
-from astropy.modeling import models, fitting, custom_model
+from astropy.modeling import Fittable1DModel,Parameter
 
-# Plotting
+from astropy.modeling import fitting
+import matplotlib.pyplot as plt
 plt.close('all')
 
-# Random seed
+# Global vars
+FLOAT_EPSILON = float(np.finfo(np.float32).tiny)
+GAUSSIAN_SIGMA_TO_FWHM = 2.0 * np.sqrt(2.0 * np.log(2.0))
+SQRT_2_PI = np.sqrt(2*np.pi)
+
+## Custom Astropy Modeling Models
+# Spectral Feature Class (Gaussian)
+class SpectralFeature(Fittable1DModel):
+
+	# Parameters
+	redshift = Parameter(default=0)
+	height	 = Parameter(default=0)
+	sigma	 = Parameter(default=1,bounds=(FLOAT_EPSILON, None)) # Must be positive
+
+	def __init__(self, center = 0, redshift = redshift.default, height = height.default,
+				sigma = sigma.default, **kwargs):
+		self.center = center
+		super().__init__(
+			redshift = redshift, height=height, sigma=sigma, **kwargs)
+		
+	@property
+	def fwhm(self):
+		"""Gaussian full sigma at half maximum."""
+		return self.sigma * GAUSSIAN_SIGMA_TO_FWHM
+
+	@property
+	def flux(self):
+		"""Gaussian flux."""
+		return self.height * self.sigma * SQRT_2_PI
+
+	def evaluate(self, x, redshift, height, sigma):
+		"""
+		Gaussian1D model function.
+		"""
+		exponand = (x - self.center*(1 + redshift)) / sigma
+		return height * np.exp(- 0.5 * exponand * exponand)
+		
+	def fit_deriv(self, x, redshift, height, sigma):
+		
+		exponand = (x - self.center*(1 + redshift)) / sigma
+		
+		d_height 	= np.exp(-0.5 * exponand * exponand)
+		d_redshift 	= height * d_height * exponand / sigma
+		d_sigma 	= height * d_height * exponand * exponand / sigma
+		
+		return [d_redshift, d_height, d_sigma]
+
+# Spectral Feature Class (Gaussian)
+class ContinuumBackground(Fittable1DModel):
+
+	# Default _param_names list; this will be filled in by the __init__
+	_param_names = ()
+
+	@property
+	def param_names(self):
+		"""Coefficient names generated based on the model's polynomial degree
+		and number of dimensions.
+			
+		Subclasses should implement this to return parameter names in the
+		desired format.
+		
+		On most `Model` classes this is a class attribute, but for polynomial
+		models it is an instance attribute since each polynomial model instance
+		can have different parameters depending on the degree of the polynomial
+		and the number of dimensions, for example.
+		"""
+
+		return self._param_names
+        
+	def __getattr__(self, attr):
+		if self._param_names and attr in self._param_names:
+			return Parameter(attr, default=0.0, model=self)
+	
+        raise AttributeError(attr)
+        
+    def __setattr__(self, attr, value):
+        # TODO: Support a means of specifying default values for coefficients
+        # Check for self._ndim first--if it hasn't been defined then the
+        # instance hasn't been initialized yet and self.param_names probably
+        # won't work.
+        # This has to vaguely duplicate the functionality of
+        # Parameter.__set__.
+        # TODO: I wonder if there might be a way around that though...
+        if attr[0] != '_' and self._param_names and attr in self._param_names:
+            param = Parameter(attr, default=0.0, model=self)
+            # This is a little hackish, but we can actually reuse the
+            # Parameter.__set__ method here
+            param.__set__(self, value)
+        else:
+            super().__setattr__(attr, value)
+        
 np.random.seed(42)
+x = np.arange(-50,50,0.1)
+g_true = SpectralFeature(center = 1, redshift = 10.1, height = 10, sigma = 5)
+y = g_true(x) + np.random.normal(loc=0,scale=1,size=x.shape)
 
-# Chi Squared
-def chi2(model,x,y,weights):
-	r = y - model(x)
-	return np.sum(r*r*weights)
-	
-# Define emission line custom model
-@custom_model
-def emissionLine(x,redshift=0,height=0,sigma=1,center=0):
-	exponand = (x - center*(1 + redshift)) # What goes in the exponent
-	return height * np.exp( -0.5 * exponand * exponand)
+g_init = SpectralFeature(center = 1, redshift = 10, height = 1, sigma = 10)
 
-# Define linear background custom model
-@custom_model
-def background(x,slope,intercept,rightedge,leftedge):
-	center 	= (rightedge + leftedge)/2
-	y 		= np.zeros(x.shape)
-	y[np.logical_and(x > rightedge,x < leftedge)] = slope*(x - center) + intercept
-	
-## Fake spectrum
-x = np.arange(4000,7000,1) # Angstroms
-y = 0.5*np.ones(x.shape) # Flux
-# Balmer
-y += models.Gaussian1D(amplitude=1, mean=6562.80, stddev=5)(x)
-y += models.Gaussian1D(amplitude=1/2.86, mean=4861.32, stddev=5)(x)
-# NII
-y += models.Gaussian1D(amplitude=1, mean=6583.34, stddev=5)(x)
-y += models.Gaussian1D(amplitude=0.34, mean=6547.96, stddev=5)(x)
-# OIII
-y += models.Gaussian1D(amplitude=1, mean=5006.77, stddev=5)(x)
-y += models.Gaussian1D(amplitude=0.35, mean=4958.83, stddev=5)(x)
-# Add error
-sigmas = np.random.uniform(0.01,0.05, x.shape)
-weights = 1/np.square(sigmas)
-y += np.random.normal(0., sigmas, x.shape)
+fit_g = fitting.LevMarLSQFitter()
+g = fit_g(g_init, x, y)
 
-emissionLines = {
-'Narrow':{
-	'OIII':([(5006.77,1),(4958.83,0.350)],0),
-	'Hbeta':([(4861.32,1)],0),
-	'NII':([(6583.34,1),(6547.96,0.340)],0),
-	'Halpha':([(6562.80,1)],0)},
-'Broad':{
-	'OIII':([(5006.77,1),(4958.83,0.350)],0),
-	'Hbeta':([(4861.32,1)],0),
-	'NII':([(6583.34,1),(6547.96,0.340)],0),
-	'Halpha':([(6562.80,1)],0)}
-}
-
-plt.step(x,y,where='mid')
+plt.plot(x,y)
+plt.plot(x,g(x))
 plt.show()
+
+
