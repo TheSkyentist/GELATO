60e996db8f5f12bbe04f1934b995f634cf0877c7
TheSkyentist <raphael.hviding@gmail.com>
2019-08-08 15:54:04 -0700

    Updates to Plotting

--- Plotting.py
+++ Plotting.py
@@ -15,6 +15,8 @@ def Plot(outfolder,name,model,full_spectrum,regions):
     # Sort regions list for good order
     regions.sort()
 
+    # Background
+    background = np.sum([model[i] for i in range(ncols)])
 
     for i,region in enumerate(regions):
 
@@ -24,17 +26,28 @@ def Plot(outfolder,name,model,full_spectrum,regions):
 
         # Axis to plot spectrum
         ax = fig.add_subplot(gs[0,i])
-        ax.set(xlim=region,ylabel='$F_\lambda$ [$10^{-17}$ erg cm$^{-2}$ s$^{-1}$ \AA$^{-1}$]')
+
+        # Plot data and model
         ax.step(wav,flux,'k')
-        ax.step(wav,model(wav),'r')
+        # Plot model components
+        for m in model[ncols:]:
+            ax.step(wav,background(wav) + m(wav))
+
+        # ax.step(wav,model(wav),'r')
+
+        # Set axis
+        ax.set(xlim=region,ylim=(0,ax.get_ylim()[1]),ylabel='$F_\lambda$ [$10^{-17}$ erg cm$^{-2}$ s$^{-1}$ \AA$^{-1}$]')
+        plt.setp(ax.get_yticklabels()[0],visible = False)
 
         # Residual Axis
         rax = fig.add_subplot(gs[1,i],sharex = ax)
         rax.step(wav,flux - model(wav),'k')
-        rax.set(xlim=region,xlabel='Wavelength [\AA]',ylabel='r')
+        ymax = np.max(np.abs(rax.get_ylim()))
+        rax.set(xlim=region,xlabel='Wavelength [\AA]',ylim=[-ymax,ymax],ylabel='r')
+
 
     # gs[1,0].add_subplot()
     fig.suptitle(figname)
-    fig.tight_layout()
+    fig.tight_layout(rect = [0, 0, 1, 0.95])
     fig.savefig(outfolder + figname + '.pdf')
     plt.close(fig)
