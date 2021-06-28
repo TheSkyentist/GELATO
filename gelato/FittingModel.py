00769d41fefc2c3fa11686264587cf1715bdb70b
TheSkyentist <raphael.hviding@gmail.com>
2021-06-28 00:38:21 -0700

    fitting updates

--- gelato/FittingModel.py
+++ gelato/FittingModel.py
@@ -160,7 +160,7 @@ def FitComponents(spectrum,cont,cont_x,emis,emis_x):
 # Fit Model
 def FitModel(model,x0,args,jac='3-point'):
     
-    fit = least_squares(fun = model.residual, jac=jac, x0 = x0, args = args,  bounds = model.get_bounds(), method='trf', ftol = 1e-7, xtol = None)
+    fit = least_squares(fun = model.residual, jac=jac, x0 = x0, args = args,  bounds = model.get_bounds(), method='trf', ftol = 1e-7, xtol = None, x_scale='jac')
 
     return fit
 
