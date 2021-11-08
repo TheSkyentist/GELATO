Added line heights, fixed test typos, added centralized units

gelato/AdditionalComponents.py
@@ -2,9 +2,7 @@
 
 import numpy as np
 import gelato.CustomModels as CM
-
-SQRT_2_PI = np.sqrt(2*np.pi)
-C = 299792.458 # km/s (sets the units of the returned values, needs to be changed in both CustomModels and AdditionalComponents)
+from gelato.Constants import C
 
 def ComponentName(index):
 
@@ -50,7 +48,8 @@ def AddComponent(flag, line, spectrum, prefix):
         model.Dispersion_bounds = (1000,10000)
 
         # Reassign Flux Bounds
-        model.Flux_bounds = (0,1.5*x0[1]*model.Dispersion_bounds[1]/oldMaxDispersion)
+        Fbound = 1.5*x0[1]*model.Dispersion_bounds[1]/oldMaxDispersion
+        model.Flux_bounds = (-Fbound,Fbound)
         
     ''' Outflow '''
     if index == 1: 

gelato/CustomModels.py
@@ -3,10 +3,10 @@
 from os import path
 import numpy as np 
 from astropy.io import fits
+from gelato.Constants import C
 
-OOSQRT_2_PI = 1/np.sqrt(2*np.pi)
-C = 299792.458 # km/s (sets the units of the returned values, needs to be changed in both CustomModels and AdditionalComponents)
 C2 = C*C
+OOSQRT_2_PI = 1/np.sqrt(2*np.pi)
 
 class CompoundModel():
 
@@ -133,9 +133,9 @@ class SpectralFeature():
 
         # Set bounds
         self.Redshift_bounds = (C*(spec.z-0.001),C*(spec.z+0.001))
-        # self.Flux_bounds = (0,1.5*Flux*self.Dispersion_bounds[1]/self.Dispersion)
-        self.Flux_bounds = (-1.5*Flux*self.Dispersion_bounds[1]/self.Dispersion,1.5*Flux*self.Dispersion_bounds[1]/self.Dispersion)
-        
+        Fbound = 1.5*Flux*self.Dispersion_bounds[1]/self.Dispersion
+        self.Flux_bounds = (-Fbound,Fbound)
+
         # Return starting value
         return np.array([spec.z*C,Flux,self.Dispersion])
 
@@ -380,7 +380,7 @@ class SSPContinuumFixed():
 
     def get_names(self):
 
-        return tuple('SSP_'+x.replace('.fits','') for x in self.ssp_names)
+        return tuple('SSP_'+x.replace('.fits','').replace('.gz','') for x in self.ssp_names)
 
 # SSP Continuum
 class SSPContinuumFree():
@@ -448,4 +448,4 @@ class SSPContinuumFree():
 
     def get_names(self):
 
-        return ('SSP_Redshift',) + tuple('SSP_'+x.replace('.fits','') for x in self.ssp_names)
+        return ('SSP_Redshift',) + tuple('SSP_'+x.replace('.fits','').replace('.gz','') for x in self.ssp_names)

gelato/EquivalentWidth.py
@@ -26,46 +26,38 @@ def EquivalentWidth(spectrum,model,parameters,param_names=None):
     else: 
         continuum = CM.CompoundModel(model.models[0:1])
 
-    # Where continuum parameters start
-    ind = continuum.nparams()
-
     # Get all continuum heights
-    continua = np.ones((parameters.shape[0],len(spectrum.wav)))
-    for i,params in enumerate(parameters):
+    continua = np.ones((len(parameters),len(spectrum.wav)))
+    for i,params in enumerate(np.array([list(p) for p in parameters])):
         continua[i] = continuum.evaluate(params,*args)
 
-    # Empty EW array
-    REWs = np.zeros((parameters.shape[0],int((parameters.shape[1]-1-ind)/3)))
+    # Iterate over lines
+    for l in ['_'.join(p.split('_')[:-1]) for p in param_names if 'Flux' in p]:
 
-    # Iterate over Emission lines
-    for i in range(ind,parameters.shape[1]-1,3):
-
-        # Get line parameters
-        param_names += (param_names[i].replace('_Redshift','_REW'),)
-
-        center = float(param_names[i].split('_')[-2]) 
-        opz = 1 + parameters[:,i]/C # 1 + z
-        flux = parameters[:,i+1] # Line flux
+        # Line Parameters
+        center = float(l.split('_')[-1])
+        opz = 1 + parameters[l+'_Redshift']/C
+        flux = parameters[l+'_Flux']
 
         # Get line
         linewav = center*opz
         linewidth = linewav*spectrum.p['LineRegion']/(2*C)
 
         # Continuum height
-        heights = np.ones(parameters.shape[0])
-        for j,lwv,lwd,ctm in zip(range(parameters.shape[0]),linewav,linewidth,continua):
+        heights = np.ones(len(parameters))
+        for j,lwv,lwd,ctm in zip(range(len(parameters)),linewav,linewidth,continua):
             # Continuum height region
             region = np.logical_and(spectrum.wav > lwv - lwd,spectrum.wav < lwv + lwd)
             if region.sum() == 0:
                 heights[j] = np.nan
             else:
                 heights[j] = np.median(ctm[region])
-    
+
         # Get REW
-        REWs[:,int((i-ind)/3)] = np.abs(flux/(heights*opz))
+        parameters.add_column(flux/(heights*opz),index=parameters.colnames.index(l+'_RHeight')+1,name=l+'_REW')
 
     # Return combined Results
-    return np.hstack((parameters,REWs)),param_names
+    return parameters
 
 # Plot from results
 def EWfromresults(params,fpath,z):
@@ -93,9 +85,6 @@ def EWfromresults(params,fpath,z):
                     parameters = parameters[[n for n in names if 'EW' not in n]]
                     names = parameters.colnames
                 break
-        
-        # Put parameters into the form we need
-        parameters = np.array([parameters[n] for n in names]).T
 
         ## Create model ##
         # Add continuum
@@ -114,10 +103,7 @@ def EWfromresults(params,fpath,z):
         model = CM.CompoundModel(models)
         
         # Calculate REWs
-        parameters,param_names = EquivalentWidth(spectrum,model,parameters,names)
-
-        # Turn into FITS table
-        parameters = Table(data=parameters,names=param_names)
+        EquivalentWidth(spectrum,model,parameters,names).write(fname,overwrite=True)
         
     if params["Verbose"]:
         print("Texture measured:",path.split('/')[-1])

gelato/gelato.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from astropy.table import Table
 
 # gelato supporting files
+from gelato.Constants import C
 import gelato.Plotting as PL
 import gelato.BuildModel as BM
 import gelato.FittingModel as FM
@@ -27,10 +28,10 @@ def gelato(params,spath,z):
     # If it exits, skip
     if path.exists(params["OutFolder"]+name.replace(".fits","-results.fits")) and not params["Overwrite"]:
         if params["Verbose"]:
-            print('gelato already exists:',name)
+            print('GELATO already exists:',name)
         return
     if params["Verbose"]:
-        print("Making gelato for",name)
+        print("Making GELATO for",name)
 
     ## Load in Spectrum ##
     if params["Verbose"]:
@@ -92,19 +93,11 @@ def gelato(params,spath,z):
         ## Plotting ##
         if params["Plotting"]:
             if params["Verbose"]:
-                print("Presenting gelato:",name)
+                print("Presenting GELATO:",name)
             # Set model parameters to median values
             PL.Plot(spectrum,model,parameters,spath)
             if params["Verbose"]:
-                print("gelato presented:",name)
-
-        ## Equivalent Widths ##
-        if params["CalcEW"]:
-            if params["Verbose"]:
-                print("Measuring texture:",name)
-            parameters,param_names = EW.EquivalentWidth(spectrum,model,parameters,param_names)
-            if params["Verbose"]:
-                print("Measured texture:",name)
+                print("GELATO presented:",name)
 
     # Otherwise:
     else:
@@ -125,10 +118,10 @@ def gelato(params,spath,z):
         ## Plotting ##
         if params["Plotting"]:
             if params["Verbose"]:
-                print("Presenting gelato:",name)
+                print("Presenting GELATO:",name)
             PL.PlotFig(spectrum,model,parameters,spath)
             if params["Verbose"]:
-                print("Gelato presented:",name)
+                print("GELATO presented:",name)
 
     # Add in continuum redshifts
     parameters = np.hstack([np.ones((len(parameters),1))*model.models[0].redshift,parameters])
@@ -139,6 +132,20 @@ def gelato(params,spath,z):
 
     # Turn into FITS table
     parameters = Table(data=parameters,names=param_names)
+
+    # Add rest line heights
+    for l in ['_'.join(p.split('_')[:-1]) for p in param_names if 'Flux' in p]:
+        center = float(l.split('_')[-1])
+        rheight = parameters[l+'_Flux']*C/(parameters[l+'_Dispersion']*center*np.sqrt(2*np.pi))
+        parameters.add_column(rheight,index=parameters.colnames.index(l+'_Dispersion')+1,name=l+'_RHeight')
+
+    ## Equivalent Widths ##
+    if params["CalcEW"]:
+        if params["Verbose"]:
+            print("Measuring texture:",name)
+        parameters = EW.EquivalentWidth(spectrum,model,parameters,param_names)
+        if params["Verbose"]:
+            print("Measured texture:",name)
 
     # Save results
     if params["Verbose"]:
@@ -172,4 +179,4 @@ def header():
     print("Started making gelato at",datetime.now())
 
 def footer():
-    print("Finished making gelato at",datetime.now())
+    print("Finished making GELATO at",datetime.now())