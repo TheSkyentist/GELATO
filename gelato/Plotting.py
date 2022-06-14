c9c60cfbbc2db173afc6a1b91b424b9ad51c2484
TheSkyentist <raphael.hviding@gmail.com>
2022-06-13 23:21:49 -0700

    Added comments

--- gelato/Plotting.py
+++ gelato/Plotting.py
@@ -173,7 +173,7 @@ def subplotplot(pt,fax,rax,spectrum,args,f):
         linelabels = np.array(linelabels)[inds]
 
         # Log barrier constraints
-        norm = 65 if pt == 0 else 15
+        norm = 65 if pt == 0 else 15 # Magic numbers for line label spacings
         x0 = np.linspace(wav.min(),wav.max(),len(inds)+2)[1:-1] # Initial guess
         linelabellocs = minimize(logbarrier,x0,args=(wav,linelocs,norm),method='Nelder-Mead',options={'adaptive':True,'maxiter':len(inds)*750}).x
 
